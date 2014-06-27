ea52cd845a98a169949ae8220579301468c212bc
Jonathan Rubin <jjr.spam@gmail.com>
2014-06-26 22:24:37 -0400

    View existing teams
    Bare bones implementation to see current members of existing teams

---
+++ TeamAlert/SCCViewTeamTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  SCCViewTeamTableViewController.h
+//  TeamAlert
+//
+//  Created by Jonathan Rubin on 6/26/14.
+//  Copyright (c) 2014 Soccer Coach Coach. All rights reserved.
+//
+
+#import "SCCTeamTableViewController.h"
+
+@interface SCCViewTeamTableViewController : SCCTeamTableViewController
+
+-(void)setDetailItem:(NSManagedObject *)team;
+
+@end
